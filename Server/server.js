992e27edd6b39daf7862aa61a0abfc1a49abf3e0
englishenotua <115879869+englishenotua@users.noreply.github.com>
2023-02-02 21:45:59 +0200

    ASDF
    ASDF

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDatabase from './config/MongoDb.js';
 import ImportData from './Dataimport.js';
+import productRoute from './Routes/ProductRoutes.js';
 
 dotenv.config();
 connectDatabase();
@@ -9,6 +10,7 @@ const app = express();
 
 //API
 app.use('/api/import', ImportData);
+app.use('/api/products', productRoute);
 
 app.get('/', (req, res) => {
   res.send('API is Running....');
