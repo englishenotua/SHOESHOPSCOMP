983a4a5832a4bfeea3a059f518bb971609b3783d
englishenotua <115879869+englishenotua@users.noreply.github.com>
2023-02-02 22:11:03 +0200

    фыва
    фыва

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDatabase from './config/MongoDb.js';
 import ImportData from './Dataimport.js';
 import productRoute from './Routes/ProductRoutes.js';
+import { errorHandler, notFound } from './Middleware/Errors.js';
 
 dotenv.config();
 connectDatabase();
@@ -12,9 +13,9 @@ const app = express();
 app.use('/api/import', ImportData);
 app.use('/api/products', productRoute);
 
-app.get('/', (req, res) => {
-  res.send('API is Running....');
-});
+//ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 1000;
 
