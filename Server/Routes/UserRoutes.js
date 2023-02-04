3046ce01ca1500c8f2ff6c7ff256991dd25ac4ef
englishenotua <115879869+englishenotua@users.noreply.github.com>
2023-02-05 01:03:10 +0200

    asdf
    asdf

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from './../Models/UserModel.js';
+import protect from './../Middleware/AuthMiddleware.js';
 
 const userRouter = express.Router();
 
@@ -24,6 +25,29 @@ userRouter.post(
     } else {
       res.status(401);
       throw new Error('Invalid Email of Password');
+    }
+  }),
+);
+
+// PROFILE
+userRouter.get(
+  '/profile',
+  protect,
+  asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
     }
   }),
 );
