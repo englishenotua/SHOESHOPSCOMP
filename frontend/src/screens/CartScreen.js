9a7aa199294122c4a1f112e1b627ba1a6248d5c3
englishenotua <115879869+englishenotua@users.noreply.github.com>
2023-02-05 23:45:29 +0200

    asdf
    asdf

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -67,7 +67,8 @@ const CartScreen = () => {
                     <h4>{item.name}</h4>
                   </Link>
                 </div>
-                <div className=" ит  col-md-2 col-sm-5 mt-md-5 mt-3 mt-md-0 d-flex">
+
+                <div className="cart-qty col-md-2 col-sm-5 mt-md-5 mt-3 mt-md-0 d-flex flex-column justify-content-center">
                   <h6>QUANTITY</h6>
                   <select
                     value={item.qty}
@@ -79,7 +80,7 @@ const CartScreen = () => {
                     ))}
                   </select>
                 </div>
-                <div className="cart-price mt-3 mt-md-0 col-md-2 align-items-sm-end">
+                <div className="cart-price mt-3 mt-md-0 col-md-2 align-items-sm-end align-items-start  d-flex flex-column justify-content-center col-sm-7">
                   <h6>PRICE</h6>
                   <h4>${item.price}</h4>
                 </div>
